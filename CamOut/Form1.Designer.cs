848685822de350a43ae17d4907e3851520a0929f
harshmaru <harsh.maru@somaiya.edu>
2016-10-23 20:19:05 +0530

    Works Most of the things
    Small app for eye Recognition

---
+++ CamOut/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace CamOut
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImgBox = new System.Windows.Forms.PictureBox();
+            this.btns = new System.Windows.Forms.Button();
+            this.btncap = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ImgBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImgBox
+            // 
+            this.ImgBox.Location = new System.Drawing.Point(1, 0);
+            this.ImgBox.Name = "ImgBox";
+            this.ImgBox.Size = new System.Drawing.Size(483, 432);
+            this.ImgBox.TabIndex = 0;
+            this.ImgBox.TabStop = false;
+            // 
+            // btns
+            // 
+            this.btns.Location = new System.Drawing.Point(412, 438);
+            this.btns.Name = "btns";
+            this.btns.Size = new System.Drawing.Size(72, 23);
+            this.btns.TabIndex = 1;
+            this.btns.Text = "Start";
+            this.btns.UseVisualStyleBackColor = true;
+            this.btns.Click += new System.EventHandler(this.btns_Click);
+            // 
+            // btncap
+            // 
+            this.btncap.Location = new System.Drawing.Point(331, 438);
+            this.btncap.Name = "btncap";
+            this.btncap.Size = new System.Drawing.Size(75, 23);
+            this.btncap.TabIndex = 2;
+            this.btncap.Text = "Capture";
+            this.btncap.UseVisualStyleBackColor = true;
+            this.btncap.Click += new System.EventHandler(this.btncap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.btncap);
+            this.Controls.Add(this.btns);
+            this.Controls.Add(this.ImgBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ImgBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ImgBox;
+        private System.Windows.Forms.Button btns;
+        private System.Windows.Forms.Button btncap;
+    }
+}
+
---
+++ CamOut/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using Emgu.CV.CvEnum;
+
+namespace CamOut
+{
+    public partial class Form1 : Form
+    {
+        private Capture cap;
+        private bool capinp;
+        private HaarCascade haar;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void FrameProcess(object sender,EventArgs e)
+        {
+            Image<Bgr, Byte> ImageFrame = cap.QueryFrame();
+            if(ImageFrame != null)
+            {
+                Image<Gray,Byte> grayframe = ImageFrame.Convert<Gray , Byte>();
+                var faces = grayframe.DetectHaarCascade(haar, 1.4, 4, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(25, 25))[0];
+                foreach(var face in faces)
+                {
+                    ImageFrame.Draw(face.rect,new Bgr(Color.Green),3);
+                }
+            }
+            ImgBox.Image = ImageFrame.ToBitmap();
+        }
+
+        private void btns_Click(object sender, EventArgs e)
+        {
+            if(cap == null)
+            {
+                    try
+                    {
+                        cap = new Capture();
+                    }
+                    catch(Exception excep)
+                    {
+                        MessageBox.Show(excep.Message);
+                    }
+            }
+            if(cap != null)
+            {
+                if (capinp)
+                {
+                    start();
+                }
+                else
+                {
+                    stop();
+                }
+                capinp = !capinp;
+            }
+        }
+        public void start()
+        {
+            btns.Text = "Start";
+            Application.Idle -= FrameProcess;
+        }
+        public void stop()
+        {
+            btns.Text = "Stop";
+            Application.Idle += FrameProcess;
+        }
+        private void btncap_Click(object sender, EventArgs e)
+        {
+            /*if(btns.Text == "Start")
+            {
+                //ImgBox.Refresh;
+                //ImgBox.Update;
+                btncap.Text = "Capture";
+                stop();
+                Image<Bgr, Byte> ImageFrame = cap.QueryFrame();
+                ImgBox.Image = ImageFrame.ToBitmap();
+                stop();
+                DialogResult result;
+                result = MessageBox.Show("Do you want to save ??","",MessageBoxButtons.YesNo);
+                if(result.ToString() == "Yes")
+                {
+                     ImageFrame.Save("E:\\harsh\\speech\\Save1.jpg");
+
+                }
+            }*/
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            haar = new HaarCascade("haarcascade_eye.xml");
+        }
+
+    }
+}
